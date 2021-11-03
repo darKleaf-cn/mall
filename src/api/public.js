235ed01aebdcbed5b00ef0896a952b460f6cd228
darKleaf <2670583526@qq.com>
2021-11-03 11:27:08 +0800

    修改axios配置

--- src/api/public.js
+++ src/api/public.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 axios.defaults.timeout = 10000
 axios.defaults.headers.post['Content-Type'] = 'application/x-www=form-urlencoded'
+axios.defaults.baseURL = process.env.VUE_APP_URL;
 export default {
   fetchGet (url, params = {}) {
-    url = process.env.VUE_APP_URL + url;
     return new Promise((resolve, reject) => {
       axios.get(url, params).then(res => {
         resolve(res.data)
@@ -13,7 +13,6 @@ export default {
     })
   },
   fetchPost (url, params = {}) {
-    url = process.env.VUE_APP_URL + url;
     return new Promise((resolve, reject) => {
       axios.post(url, params).then(res => {
         resolve(res.data)
